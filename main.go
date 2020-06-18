9cd5b0fd425976d41efa85954c98f106de7aae7c
André Costa <andreccosta@me.com>
2020-06-18 23:39:19 +0100

    Add initial stop/update/start logic

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 var (
@@ -13,8 +17,53 @@ var (
 func main() {
 	flag.Parse()
 
+	ctx := context.Background()
+	containerID := flag.Arg(0)
+
 	// Combine results from full flag and short flag
 	shouldUpdateImageFlag := *shortUpdateImgFlag || *updateImgFlag
 
-	fmt.Println("Combined flag value", shouldUpdateImageFlag)
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	container, err := cli.ContainerInspect(ctx, containerID)
+	if err != nil {
+		panic(err)
+	}
+
+	isRunning := container.State.Running || container.State.Paused
+	imageName := container.Config.Image
+
+	// if container is set to autoremove stopping it would completely remove it
+	if container.HostConfig.AutoRemove {
+		panic(fmt.Errorf("container %s is set to autoremove", container.Name))
+	}
+
+	if isRunning {
+		fmt.Println("Stopping container ...")
+
+		err = cli.ContainerStop(ctx, containerID, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if shouldUpdateImageFlag {
+		fmt.Println("Updating container image ...")
+		out, err := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		defer out.Close()
+	}
+
+	fmt.Println("Starting container ...")
+
+	err = cli.ContainerStart(ctx, containerID, types.ContainerStartOptions{})
+	if err != nil {
+		panic(err)
+	}
 }
